a48f828ed2b9b4fb37a8004b014f95741f016cb7
LukaszKur <lukaszkurasz2@gmail.com>
2018-01-26 11:10:37 +0100

    Zliczanie małe, duże ciag_sin_popraw

--- ciag_sinus.c
+++ ciag_sinus.c
@@ -3,25 +3,29 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+
 #define DOKLADNOSC 50
-int main(int liczba_slow, char* tablica_slow[])
-{
-    long double x=3;
-    long int jed=1;
-    long double wynik=0.0;
-    long double silnia=1.0;
-    long double potega_x=1.0;
-    //if(liczba_slow==3) { //pierwszy parametr to nazwa programu
-       // x = atof(tablica_slow[1]);
+
+int main(int liczba_slow, char *tablica_slow[]) {
+    double x;
+    int jed = 1;
+    double wynik = 0.0;
+    double silnia = 1.0;
+    double potega_x = 1.0;
+    if (liczba_slow == 2) { //pierwszy parametr to nazwa programu
+        x = atof(tablica_slow[1]);
+        printf("x=%lf\n", x);
+
         for (int licznik = 1; licznik < DOKLADNOSC; ++licznik) {
             ++jed;
-            potega_x *= (x*x);
-            silnia*=(licznik*licznik);
-            wynik += (jed%2==0?1:-1)*potega_x / silnia;
+            potega_x *= (x * x);
+            silnia *= (licznik * licznik);
+            wynik += (jed % 2 == 0 ? 1 : -1) * potega_x / silnia;
         }
-        printf("Wynik: %Lf", wynik);
-    //}else{
-     //   printf("Nie poprawna liczba parametrow!\n");
-    //}
+        printf("Wynik: %lf", wynik);
+
+    } else {
+        printf("Nie poprawna liczba parametrow!\n");
+    }
     return 0;
 }
